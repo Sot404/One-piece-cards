34ae8292c2d62a5619226341d3601b5273f3938c
Sot404 <sotirisioannou1803@gmail.com>
2026-01-09 21:31:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { setCurrentTab, setTotalPreference, setSortMode } from './state.js';
 import { render } from './render.js';
 import { openAddModal } from './modal.js';
-import { joinRoom } from './firebase.js';
+import { joinRoom, mergeFromJSON } from './firebase.js';
 import { exportToJSON } from './export.js';
+import { mergeFromJSON } from './merge.js';
 
 /* =========================
    INITIALIZATION
@@ -12,6 +13,8 @@ import { exportToJSON } from './export.js';
 // Μπορείς αργότερα να το κάνεις input
 joinRoom('DEFAULT');
 
+document.getElementById('mergeBtn').onclick = mergeFromJSON;
+document.getElementById('mergeBtn').onclick = mergeFromJSON;
 document.getElementById('exportBtn').onclick = exportToJSON;
 
 /* =========================
