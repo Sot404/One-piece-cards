1ae78a81cc077d722c1532b3eb8ee854e2501157
Sot404 <sotirisioannou1803@gmail.com>
2026-01-09 21:29:54 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -35,4 +35,28 @@ export function saveToFirebase() {
     Marira: data.Marira,
     Soriris: data.Soriris
   });
+   
+export async function mergeFromRoom(roomCode) {
+  const refRoom = ref(window.firebaseDB, `rooms/${roomCode}`);
+  const snap = await get(refRoom);
+
+  if (!snap.exists()) {
+    alert('Room not found');
+    return;
+  }
+
+  const incoming = snap.val();
+
+  ['Marira', 'Soriris'].forEach(tab => {
+    for (const key in incoming[tab]) {
+      if (!data[tab][key]) {
+        data[tab][key] = incoming[tab][key];
+      }
+    }
+  });
+
+  saveToFirebase();
+  render();
+}
+   
 }
