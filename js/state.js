bcb10555ac51640d70fb019fa4f7504d438560e3
Sot404 <sotirisioannou1803@gmail.com>
2026-01-09 13:20:21 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,26 @@
+export const STORAGE_KEY = 'interactive-list-save';
+
+export let data = {
+  Marira: {},
+  Soriris: {}
+};
+
+export let currentTab = 'Marira';
+export let totalPreference = 'Marira';
+
+export function setCurrentTab(tab) {
+  currentTab = tab;
+}
+
+export function setTotalPreference(tab) {
+  totalPreference = tab;
+}
+
+export function save() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+export function load() {
+  const saved = localStorage.getItem(STORAGE_KEY);
+  if (saved) data = JSON.parse(saved);
+}
