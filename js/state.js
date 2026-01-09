f9f3db913e3dab0f97b5d38b2664a5919437cdbf
Sot404 <sotirisioannou1803@gmail.com>
2026-01-09 18:19:32 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,5 +1,3 @@
-export const STORAGE_KEY = 'interactive-list-save';
-
 export let data = {
   Marira: {},
   Soriris: {}
@@ -7,8 +5,7 @@ export let data = {
 
 export let currentTab = 'Marira';
 export let totalPreference = 'Marira';
-export let sortMode = 'number'; 
-// 'number' | 'count'
+export let sortMode = 'number'; // 'number' | 'count'
 
 export function setSortMode(mode) {
   sortMode = mode;
@@ -20,13 +17,4 @@ export function setCurrentTab(tab) {
 
 export function setTotalPreference(tab) {
   totalPreference = tab;
-}
-
-export function save() {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-}
-
-export function load() {
-  const saved = localStorage.getItem(STORAGE_KEY);
-  if (saved) data = JSON.parse(saved);
 }
